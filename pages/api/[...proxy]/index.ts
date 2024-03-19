e4d5d7d93b989e1b823ed05608ff4ee603a051e2
Jordan Jantschulev <j.jantschulev@gmail.com>
2024-03-19 14:14:02 -0400

    add logs

--- pages/api/[...proxy]/index.ts
+++ pages/api/[...proxy]/index.ts
@@ -7,9 +7,12 @@ export const config = {
 export default async function handler(req: NextRequest, _ev: NextFetchEvent) {
   const originalUrl = new URL(req.url);
   const origin = originalUrl.searchParams.get("origin");
+  console.log("log 0");
   if (!origin) return new Response("?origin not provided", { status: 400 });
+  console.log("log 1");
   const url = new URL(originalUrl.pathname.slice(4), origin);
   url.search = originalUrl.search;
+  console.log("log 2", url.href);
   const forwardReq = new Request(url, {
     method: "POST",
     body: JSON.stringify({ message: "g'day mate!" }),
@@ -17,8 +20,11 @@ export default async function handler(req: NextRequest, _ev: NextFetchEvent) {
       "Content-Type": "application/json",
     },
   });
+  console.log("log 3");
   const res = await fetch(forwardReq);
+  console.log("log 4");
   res.headers.set("x-forwarded-url", url.href);
   res.headers.set("x-original-url", originalUrl.href);
+  console.log("log 5");
   return res;
 }
