8c749b649116b08475e2fd38e51cea164b0145b9
Jordan Jantschulev <j.jantschulev@gmail.com>
2024-03-19 12:57:09 -0400

    update

--- pages/api/[...proxy]/index.ts
+++ pages/api/[...proxy]/index.ts
@@ -8,7 +8,7 @@ export default async function handler(req: NextRequest, _ev: NextFetchEvent) {
   const originalUrl = new URL(req.url);
   const origin = originalUrl.searchParams.get("origin");
   if (!origin) return new Response("?origin not provided", { status: 400 });
-  const url = new URL(originalUrl.pathname, origin);
+  const url = new URL(originalUrl.pathname.slice(4), origin);
   url.search = originalUrl.search;
   const newReq = new Request(url, req);
   const res = await fetch(newReq);
