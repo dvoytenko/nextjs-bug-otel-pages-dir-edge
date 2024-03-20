23effd15e95d2e19e1c921e0b439c4d7dbc0ed2f
dvoytenko <dima.voytenko@vercel.com>
2024-03-20 11:52:47 -0700

    back to req

--- pages/api/[...proxy]/index.ts
+++ pages/api/[...proxy]/index.ts
@@ -13,21 +13,15 @@ export default async function handler(req: NextRequest, _ev: NextFetchEvent) {
   const url = new URL(originalUrl.pathname.slice(4), origin);
   url.search = originalUrl.search;
   console.log("log 2", url.href);
-  // const forwardReq = new Request(url, {
-  //   method: "POST",
-  //   body: JSON.stringify({ message: "g'day mate!" }),
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   },
-  // });
-  console.log("log 3");
-  const res = await fetch(url, {
+  const forwardReq = new Request(url, {
     method: "POST",
     body: JSON.stringify({ message: "g'day mate!" }),
     headers: {
       "Content-Type": "application/json",
     },
   });
+  console.log("log 3");
+  const res = await fetch(forwardReq);
   console.log("log 4");
   res.headers.set("x-forwarded-url", url.href);
   res.headers.set("x-original-url", originalUrl.href);
